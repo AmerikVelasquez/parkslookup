3029cefb87db8758982e4d2442cb03c5d0b19845
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-11-24 16:41:20 -0800

    fixed naming

--- ParksLookup/Program.cs
+++ ParksLookup/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace ParksLookup.Solution
+namespace ParksLookup
 {
     public class Program
     {
--- ParksLookup/Startup.cs
+++ ParksLookup/Startup.cs
@@ -1,18 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using ParksLookup.Models;
 
-namespace ParksLookup.Solution
+namespace ParksLookup
 {
     public class Startup
     {
@@ -26,12 +20,10 @@ namespace ParksLookup.Solution
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ParksLookup.Solution", Version = "v1" });
-            });
+             services.AddDbContext<ParksLookupContext>(opt =>
+                    opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+                    services.AddControllers();
+        
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,11 +32,10 @@ namespace ParksLookup.Solution
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ParksLookup.Solution v1"));
+               
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
