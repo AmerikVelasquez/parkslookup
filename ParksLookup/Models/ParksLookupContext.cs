b939fdd2873c281740120f73d847649d90ceebec
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-11-24 17:34:35 -0800

    add model builder to db context

--- ParksLookup/Models/ParksLookupContext.cs
+++ ParksLookup/Models/ParksLookupContext.cs
@@ -9,5 +9,17 @@ namespace ParksLookup.Models
       {
       }
       public DbSet<Park> Parks {get;set;} 
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Park>()
+        .HasData(
+          new Park { ParkId = 1, Name= "Big Hole", Type = "National Park", Location= "Montana" },
+          new Park { ParkId = 2, Name= "Fort Vancouver", Type = "National Park", Location= "Washington" },
+          new Park { ParkId = 3, Name= "Eleanor Roosevelt", Type = "National Park", Location= "New York" },
+          new Park { ParkId = 4, Name= "Goldendale Observatory", Type = "State Park", Location= "Washington" },
+          new Park { ParkId = 5, Name= "Big Hole", Type = "National Park", Location= "Montana" }
+        );
+    }
   }
 }
